0fe1ec2137039e66e239760c28b4d9552a6aef46
connordennyomalley <67078108+connordennyomalley@users.noreply.github.com>
2024-02-22 21:04:48 +0000

    Initial Dotfiles
    zsh and neovim

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,11 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "scala", "sbt", "java" },
+  callback = function()
+    require("metals").initialize_or_attach({})
+  end,
+  group = nvim_metals_group,
+})
---
+++ .config/nvim/lua/plugins/metals.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "scalameta/nvim-metals",
+  },
+}
